8502bde49b78c8995525546f3a4302dcb4c67986
DonkeyTomy <42727777+DonkeyTomy@users.noreply.github.com>
2020-12-09 18:00:21 +0800

    更新Hilt及Kotlin版本

--- view_library/src/main/java/com/tomy/lib/ui/activity/BaseApplication.kt
+++ view_library/src/main/java/com/tomy/lib/ui/activity/BaseApplication.kt
@@ -2,13 +2,11 @@ package com.tomy.lib.ui.activity
 
 import android.app.Application
 import com.zzx.utils.TTSToast
-import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 /**@author Tomy
  * Created by Tomy on 2018/11/5.
  */
-@HiltAndroidApp
 open class BaseApplication: Application() {
 
     override fun onCreate() {
