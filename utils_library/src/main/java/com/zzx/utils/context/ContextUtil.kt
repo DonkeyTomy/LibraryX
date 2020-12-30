059f2b34287134c0efd34a43946aeed48dae1bc6
DonkeyTomy <42727777+DonkeyTomy@users.noreply.github.com>
2020-12-30 16:26:44 +0800

    ContextUtil.kt 增加获取该类泛型类

--- utils_library/src/main/java/com/zzx/utils/context/ContextUtil.kt
+++ utils_library/src/main/java/com/zzx/utils/context/ContextUtil.kt
@@ -6,8 +6,11 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import timber.log.Timber
+import java.lang.reflect.ParameterizedType
 
 /**@author Tomy
  * Created by Tomy on 2018/7/10.
@@ -129,4 +132,54 @@ object ContextUtil {
         }
     }
 
+}
+
+/**
+ * class.java.genericSuperClass只会获取该类的泛型类而不会获取其父类所拥有的泛型类.
+ * 因此在多层子类中出现某个子类已实现了泛型而再往下的子类不再实现时就需要单独寻找该子类的泛型参数
+ * @receiver Son
+ * @param Father 判断是否是父类
+ * @param Son 判断是否是[Father]的子类.调用此方法的对象本身的类或者父类即可
+ * @param inflater LayoutInflater
+ * @param container ViewGroup?
+ * @return VB?
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified Father, Son: Any, reified VB> Son.getViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB? {
+    Timber.d("Father = ${Father::class.java.simpleName}; Son = ${this.javaClass.simpleName}; VB = ${VB::class.java.simpleName}")
+    if (Father::class.java.isAssignableFrom(this.javaClass)) {
+        Timber.d("isAssignableFrom()")
+        (Father::class.java.genericSuperclass as ParameterizedType).actualTypeArguments.iterator().forEach {
+            Timber.d("type = ${it::class.java.simpleName}")
+            val clazz = it as Class<*>
+            Timber.v("clazz = ${clazz.simpleName}")
+            if (clazz == VB::class.java) {
+                val method = clazz.getDeclaredMethod(
+                        "inflate",
+                        LayoutInflater::class.java,
+                        ViewGroup::class.java,
+                        Boolean::class.java
+                )
+                Timber.v("load Base ViewBinding")
+                return method.invoke(null, inflater, container, false) as VB
+            }
+        }
+    }
+    Timber.d("is not AssignableFrom()")
+    val type    = this.javaClass.genericSuperclass as ParameterizedType
+    type.actualTypeArguments.iterator().forEach {
+        val aClass  = it as Class<*>
+//            if (!ViewDataBinding::class.java.isAssignableFrom(aClass) && ViewBinding::class.java.isAssignableFrom(aClass)) {
+        if (aClass == VB::class.java) {
+            val method = aClass.getDeclaredMethod(
+                    "inflate",
+                    LayoutInflater::class.java,
+                    ViewGroup::class.java,
+                    Boolean::class.java
+            )
+            Timber.v("load ViewBinding")
+            return method.invoke(null, inflater, container, false) as VB
+        }
+    }
+    return null
 }
