d9a2e63e4b1448add90652431cf19f86251832c3
DonkeyTomy <42727777+DonkeyTomy@users.noreply.github.com>
2022-08-31 17:30:24 +0800

    update

---
+++ compose/src/main/java/com/tomy/compose/fragment/BaseAdapterComposeFragment.kt
@@ -0,0 +1,13 @@
+package com.tomy.compose.fragment
+
+import androidx.compose.runtime.Composable
+
+/**@author Tomy
+ * Created by Tomy on 2022/8/31.
+ */
+class BaseAdapterComposeFragment: BaseComposeFragment() {
+
+    @Composable
+    override fun CreateContent() {
+    }
+}
