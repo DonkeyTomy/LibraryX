f37f85bb28d11d92a8345f809a3684b7f8d6add3
DonkeyTomy <42727777+DonkeyTomy@users.noreply.github.com>
2023-08-07 23:41:32 +0800

    update

--- compose/src/main/java/com/tomy/compose/components/button/CustomButtons.kt
+++ compose/src/main/java/com/tomy/compose/components/button/CustomButtons.kt
@@ -1,17 +1,32 @@
 package com.tomy.compose.components.button
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CircleIconButton(
@@ -19,17 +34,57 @@ fun CircleIconButton(
     @DrawableRes
     iconRes: Int,
     onClick: () -> Unit,
+    backgroundColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    iconColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
     enable: Boolean = true
 ) {
-    Button(
-        modifier = modifier,
-        shape = RoundedCornerShape(100),
+
+    IconButton(
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(backgroundColor)
+            .then(modifier),
         enabled = enable,
         onClick = onClick,
     ) {
-        Image(
-            painter = painterResource(id = iconRes), contentDescription = "$iconRes"
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = "$iconRes",
+            tint = iconColor
         )
-        Text(text = "123")
+    }
+}
+
+@Composable
+fun IconButtonWithTxt(
+    modifier: Modifier = Modifier,
+    @DrawableRes
+    iconRes: Int,
+    onClick: () -> Unit,
+    backgroundColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    iconColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
+    enable: Boolean = true,
+    @StringRes
+    txtRes: Int,
+    textStyle: TextStyle = LocalTextStyle.current
+) {
+    Column(
+        modifier = Modifier.wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircleIconButton(
+            modifier = modifier,
+            iconRes = iconRes,
+            onClick = onClick,
+            backgroundColor = backgroundColor,
+            iconColor = iconColor,
+            enable = enable
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = stringResource(id = txtRes),
+            maxLines = 1,
+            style = textStyle
+        )
     }
 }
